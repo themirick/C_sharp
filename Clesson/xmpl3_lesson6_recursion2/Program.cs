5e0a59d4cc106afc202b2f5b96fe3ed12a648dab
themirick <themirick.@gmail.com>
2023-02-01 19:17:33 +0500

    Trained some exaples from lection 7

---
+++ Clesson/xmpl3_lesson6_recursion2/Program.cs
@@ -0,0 +1,11 @@
+﻿string path = "D:/C_sharp/Clesson/xmpl2_dimensional_arrays_recursion";
+DirectoryInfo di = new DirectoryInfo(path);
+
+System.Console.WriteLine(di.CreationTime);
+
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}

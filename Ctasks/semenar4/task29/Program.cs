9236e6ed639be94c9599a9c2c25d4b5a97c7d372
themirick <themirick.@gmail.com>
2023-02-05 21:24:37 +0500

    done task #29

---
+++ Ctasks/semenar4/task29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int GetNumber(string massage)
+{
+    System.Console.Write(massage);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int[] MakeArray(int arraySize)
+{
+    int [] newArray = new int[arraySize];
+    for (int i = 0; i < arraySize; i++)
+    {
+        newArray[i] = new Random().Next(1, 10);
+    }
+    return newArray;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+}
+
+int arraySize = GetNumber("Inter needed size of array>> ");
+PrintArray(MakeArray(arraySize));

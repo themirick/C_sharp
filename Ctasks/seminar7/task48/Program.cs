81d5117d09c17678499a8e591c29474e38bef121
themirick <themirick.@gmail.com>
2023-02-16 01:18:40 +0500

    done task #48

---
+++ Ctasks/seminar7/task48/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый 
+// элемент в массиве находится по формуле: Aij = i+J. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+void FillArray2D(int[,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            array2D[i, j] = i + j;
+        }
+    }
+}
+
+void PrintArray2D(int[,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            System.Console.Write(array2D[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int rows = 3;
+int columns = 4;
+
+int[,] array2D = new int[rows, columns];
+FillArray2D(array2D);
+PrintArray2D(array2D);

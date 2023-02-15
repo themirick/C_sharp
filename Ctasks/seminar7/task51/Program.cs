6671c1955a09dcb77ee669fc8267825cd1f65997
themirick <themirick.@gmail.com>
2023-02-16 01:19:20 +0500

    done task #51

---
+++ Ctasks/seminar7/task51/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+
+void FillArray2D(int[,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            array2D[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            System.Console.Write($"{array2D[i, j], -2}");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void MachingMatrixDiagonal(int[,] array2D)
+{
+    int counter = 0;
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            if (i == j) counter += array2D[i,j];
+        }
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Summ of main diagonal of the matrix>> {counter}");
+}
+
+int[,] array2D = new int[6, 8];
+
+FillArray2D(array2D);
+PrintArray2D(array2D);
+MachingMatrixDiagonal(array2D);

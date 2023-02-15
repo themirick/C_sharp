137b9bc2ce149999c098481afb886dbe3eae317b
themirick <themirick.@gmail.com>
2023-02-16 01:21:28 +0500

    done task #47

---
+++ Ctasks/seminar7/task47/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void FillArray2D(double[,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            array2D[i, j] = new Random().NextDouble() * new Random().Next(-100, 100);
+        }
+    }
+}
+
+void PrintArray2D(double[,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            System.Console.Write($"{array2D[i, j],7:F1} |");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[,] array2D = new double[7, 4];
+
+FillArray2D(array2D);
+PrintArray2D(array2D);

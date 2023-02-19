4a34cb045ef2afe484b265c53597c34582d19856
Mirsoat <themirick@gmail.com>
2023-02-19 19:02:22 +0500

    Done task #65

---
+++ Ctasks/seminar9/task65/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> "1, 2, 3, 4, 5" 
+// M = 4; N = 8. -> "4, 5, 6, 7, 8"
+
+
+void ListNumbers(int startingNumber, int limitingNumber)
+{
+    if (startingNumber > limitingNumber)
+        return;
+
+    System.Console.WriteLine(startingNumber);
+    ListNumbers(++startingNumber, limitingNumber);
+}
+
+
+ListNumbers(405, 500);

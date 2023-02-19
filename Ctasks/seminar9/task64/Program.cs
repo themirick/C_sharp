ccb8cdaed3bafd5bcedb3d4e40a820a087a61a7c
Mirsoat <themirick@gmail.com>
2023-02-19 23:17:35 +0500

    Done task #64

---
+++ Ctasks/seminar9/task64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void ListNumbers(int listLimit)
+{
+    if (listLimit < 1) return;
+    System.Console.Write($"{listLimit}, ");
+    ListNumbers(listLimit-1);
+}
+
+ListNumbers(5);

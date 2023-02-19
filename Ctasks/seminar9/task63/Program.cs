1bfa3be75e4421ddc5e0cf8226eb363b3a4eca62
Mirsoat <themirick@gmail.com>
2023-02-19 18:52:26 +0500

    Done task #63

---
+++ Ctasks/seminar9/task63/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке 
+// от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+
+void ListNumbersRec(int currentIndex, int listLimit)
+{
+    if (currentIndex == listLimit + 1)
+        return;
+
+    System.Console.WriteLine(currentIndex);
+    ListNumbersRec(++currentIndex, listLimit);
+}
+
+ListNumbersRec(1, 5);

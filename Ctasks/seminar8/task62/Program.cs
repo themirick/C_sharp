f417d220a9565e31b407e6afe07e3e35ca66c708
Mirsoat <themirick@gmail.com>
2023-02-19 15:36:33 +0500

    Task #62 isn't finished yet

---
+++ Ctasks/seminar8/task62/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 62. Напишите программу, которая заполнит 
+// спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+
+
+// The project is under development yet
+// Проект всё ещё на стадии разработки
+
+
+
+void FillMatrixSpirally(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            matrix[i, j] = 
+        }
+    }
+}
+
+void PrintArray2D(int[,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            System.Console.Write($"{array2D[i, j],3}");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+
+int[,] array2D = new int[4, 4];
+
+FillArray2D(array2D);
+PrintArray2D(array2D);

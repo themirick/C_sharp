51ce194c448e855ed02ff8ced8e729fe1af851a6
Mirsoat <themirick@gmail.com>
2023-02-19 22:11:53 +0500

    Done task #67

---
+++ Ctasks/seminar9/task67/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 67: Напишите программу, которая будет 
+// принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+int SumDigitsOfNumber(int anynumber)
+{
+    if (anynumber < 10) return anynumber;
+    
+    return anynumber % 10 + SumDigitsOfNumber(anynumber / 10);
+}
+
+System.Console.WriteLine($"Summ of the digits in number is>> {SumDigitsOfNumber(1222)}");
